cc13f53bcb6fc4c8e132f9e48388e08bcf5c59b4
Fernando Judice <fernandocesar95@gmail.com>
2024-04-22 22:29:34 -0300

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(a,b) {
 
+    if (typeof a !== 'number' || typeof b !== 'number' || a<0||b<0) {
+        return "ERROR"
+    }
+
+    let result = 0;
+    for (let i=Math.min(a,b); i<=Math.max(a,b); i++){
+        result += i;
+    }
+    return result
 };
 
 // Do not edit below this line
