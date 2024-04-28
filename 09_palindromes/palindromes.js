584751bbf4070575ea675b2465e443cb4e1a78b6
Fernando Judice <fernandocesar95@gmail.com>
2024-04-27 21:20:00 -0300

    add solution 09 palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
+const palindromes = function (word) {
 
+    cleanWord = word.replace(/[^\w]/gi,'').toLowerCase();
+    return cleanWord === cleanWord.split('').reverse().join('')
 };
 
 // Do not edit below this line
