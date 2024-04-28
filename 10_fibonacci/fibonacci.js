e6d1761ccb5511a953425632925a50a31cb9cb1d
Fernando Judice <fernandocesar95@gmail.com>
2024-04-27 21:27:36 -0300

    add solution exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
+const fibonacci = function(index) {
 
+    let indexNumber = Number(index);
+    if (indexNumber<0) return "OOPS"
+    if (indexNumber === 0) return 0
+    if (indexNumber <= 2){
+        return 1;
+    } else {
+        return fibonacci(indexNumber-1) + fibonacci(indexNumber-2)
+    }
 };
 
 // Do not edit below this line
